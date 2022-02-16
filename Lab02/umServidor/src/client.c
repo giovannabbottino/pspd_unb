701ccf33c2294fcf7da6be35a7a3f5f83a9409aa
giovannabbottino <giovannabbottino@gmail.com>
2022-02-16 11:01:13 -0300

    Adiciona Lab02 parte um servidor
    Co-authored-by: fealps <felipeboccardi@hotmail.com>

---
+++ Lab02/umServidor/src/client.c
@@ -0,0 +1,73 @@
+/*
+* Giovanna Borges Bottino 170011267
+* Felipe Boccardi Silva Agustini 180119818
+*/
+
+#include <properties.h> /* informacoes para rodar */
+
+int main(int argc, char *argv[]) {
+	int client;
+	struct sockaddr_in server_address; /* socket do servidor */
+	float vetor[VETOR], menor = 0, maior = 0, receive_message[2];
+
+	/* Cria e descobre o menor e maior valor*/
+	for (int i=0; i<VETOR; i++){
+		vetor[i] = pow((i - VETOR / 2), 2);
+		vetor[i] = sqrt(vetor[i]);
+
+		if (vetor[i] >= maior){
+			maior = vetor[i];
+		}
+		if (vetor[i] <= menor){
+			menor = vetor[i];
+		}
+	}
+
+	/* Criacao do socket TCP */
+	client = socket(AF_INET, SOCK_STREAM, 0);
+	if (client == 0){ 
+		/*
+		* ARPA INTERNET PROTOCOLS -- IPv4
+		* SOCK_STREAM orientado a conexão com transmissão de fluxos de bytes, sequencial e bidirecional
+		* 0 - protocolo padrao para o tipo escolhido -- TCP
+		*/
+		perror("[CLIENT] Criação do socket falhou");
+		exit(0);
+	}
+	printf("[CLIENT] Criacao do socket TCP\n");
+
+	/* Preenchendo informacoes sobre o cliente */
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(HOST); 
+	server_address.sin_port = htons(PORT);
+
+	/* Inicia a conexão no socket */
+	if (connect(client, (struct sockaddr *)&server_address, sizeof(server_address))<0){
+		perror("[CLIENT] Não pode conectar no Socket");
+		exit(0);
+	}
+	printf("[CLIENT] Inicia a conexão no socket\n");
+
+	printf("[CLIENT] Conectado no IP: %s, porta TCP numero: %d\n", HOST, PORT);
+	
+	/* Verifica se mensagem foi enviada */
+	while(1){
+
+		if(send(client, &vetor, VETOR * sizeof(float), 0) < 0){
+			perror("[CLIENT] Falha no envio");
+		} else{
+			printf("[CLIENT] Mensagem enviada\n");
+		}
+
+		if(recv(client, &receive_message, 2 * sizeof(float), 0) < 0){
+			perror("[CLIENT] Falha ao receber resposta");
+		}
+
+		if (menor == receive_message[0] && maior == receive_message[1]){
+			printf("[CLIENT] Menor valor recebido está correto: %f\n", receive_message[0]);
+			printf("[CLIENT] Maior valor recebido está correto: %f\n", receive_message[1]);
+			close(client);
+			exit(0);
+		}
+	}
+}
---
+++ Lab02/umServidor/src/server.c
@@ -0,0 +1,87 @@
+/*
+* Giovanna Borges Bottino 170011267
+* Felipe Boccardi Silva Agustini 180119818
+*/
+#include <properties.h>
+
+int main(int argc, char *argv[]){
+	int server, client;
+	socklen_t client_size;
+	float menor = 0, maior = 0, client_number;
+	float vetor[VETOR], send_message[2];
+
+	struct sockaddr_in server_address, client_address; /* socket do servidor e cliente  */
+
+	/* Criacao do socket TCP */
+	server = socket(AF_INET, SOCK_STREAM, 0);
+	if ( server == 0){ 
+		/*
+		* ARPA INTERNET PROTOCOLS -- IPv4
+		* SOCK_STREAM orientado a conexão com transmissão de fluxos de bytes, sequencial e bidirecional
+		* 0 - protocolo padrao para o tipo escolhido -- TCP
+		*/
+		perror("[SERVER] Criação do socket falhou");
+		exit(0);
+	}
+	printf("[SERVER] Criacao do socket TCP\n");
+
+	/* Preenchendo informacoes sobre o servidor */
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(HOST); 
+	server_address.sin_port = htons(PORT);
+
+	/* Conecta na porta */
+	if (bind(server, (struct sockaddr *)&server_address, sizeof(server_address))<0){
+		perror("[SERVER] Não pode fazer bind na porta PORT");
+		exit(0);
+	}
+	printf("[SERVER] Conecta na porta\n");
+
+	/*  numero maximo de conexões */
+	listen(server, 5);
+	printf("[SERVER] Escutando\n");
+
+	/* Aceita conexoes direta entre o servidor e cliente */
+	client_size = sizeof(client_address);
+	client = accept(server, (struct sockaddr *) &client_address, &client_size);
+	if ( client == -1 ){
+		perror("[SERVER] Não pode dar accept na PORT");
+		exit(0);
+	} 
+	printf("[SERVER] Aceita conexoes direta\n");
+
+	printf("[SERVER] Esperando por dados no IP: %s, porta TCP numero: %d\n", HOST, PORT);
+	int retorno_msg;
+	/* Receber a mensagem */
+	while(1){		
+		retorno_msg = recv(client, &vetor, VETOR * sizeof(float), 0);
+		
+		if(retorno_msg == 0 || errno == 104){
+			printf("[SERVER] Conexão Encerrada!\n");
+			close(client);
+			exit(0);
+		}else if (retorno_msg < 0){
+			perror("[SERVER] Não foi possivel receber a mensagem");
+		} else{
+			printf("[SERVER] Recebida a mensagem\n");
+
+			/* para responder */
+			/* Descobre o menor e maior valor*/
+			for (int i=0; i<VETOR; i++){
+				if (vetor[i] >= maior){
+					maior = vetor[i];
+				}
+				if (vetor[i] <= menor){
+					menor = vetor[i];
+				}
+			}
+
+			send_message[0] = menor;
+			send_message[1] = maior;
+
+			if (send(client, &send_message, VETOR * sizeof(float) + 1, 0) < 0){
+				perror("[SERVER] Não foi possivel responder a mensagem");
+			}
+		}
+	}
+}
